4dc06f2425c60316e046775adc856d4ee2be5759
matio <1002438709@qq.com>
2016-03-15 20:15:47 +0800

    do example

--- app/src/main/java/com/angel/present_say/fragment/StrategyHeaderFragment.java
+++ app/src/main/java/com/angel/present_say/fragment/StrategyHeaderFragment.java
@@ -1,10 +1,13 @@
 package com.angel.present_say.fragment;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.alibaba.fastjson.JSONObject;
 import com.angel.present_say.R;
+import com.angel.present_say.activity.CategoryChildActivity;
 import com.angel.present_say.base.BaseFragment;
 import com.angel.present_say.bean.CategorySubject;
 import com.angel.present_say.common.CategoryConstant;
@@ -20,10 +23,12 @@ import java.util.List;
  * Created by Angel on 2016/2/15.
  */
 @ContentView(R.layout.header_category_layout)
-public class StrategyHeaderFragment extends BaseFragment {
+public class StrategyHeaderFragment extends BaseFragment implements View.OnClickListener {
 
     @ViewInject(value = R.id.ad_header_category)
     private TabLayout subjectTab;
+
+    private int mId;
 
     public static StrategyHeaderFragment newInstance() {
         StrategyHeaderFragment fragment = new StrategyHeaderFragment();
@@ -34,7 +39,7 @@ public class StrategyHeaderFragment extends BaseFragment {
 
     @Override
     public void requestNetData() {
-        xHttpUtils.get(CategoryConstant.AD_URL_GET,this);
+        xHttpUtils.get(CategoryConstant.AD_URL_GET, this);
     }
 
     @Override
@@ -50,14 +55,34 @@ public class StrategyHeaderFragment extends BaseFragment {
     @Override
     public void get(String result) {
         CategorySubject subject = JSONObject.parseObject(result, CategorySubject.class);
+
         if (subject != null) {
+
             List<CategorySubject.CategoryData.CategoryCollections> collections = subject.getData().getCollections();
-            //设置TabLayout文本
+
+            //设置TabLayout
             for (int i = 0, size = collections.size(); i < size; i++) {
+
                 ImageView imageView = new ImageView(getActivity());
+
+                mId = collections.get(i).getId();
+                imageView.setOnClickListener(this);
+
                 x.image().bind(imageView, collections.get(i).getBanner_image_url());
+
                 subjectTab.addTab(subjectTab.newTab().setCustomView(imageView));
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //http://api.liwushuo.com/v2/collections/223/posts?limit=20&offset=0
+        Intent intent = new Intent(getActivity(), CategoryChildActivity.class);
+        intent.putExtra("id", mId);
+        startActivity(intent);
+
+
     }
 }
